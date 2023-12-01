03e083e09a8af96bf60621d20090394aa00807d2
Eidepolcycne <fbab1@free.fr>
2023-12-01 09:34:45 +0100

    del death code

--- index.js
+++ index.js
@@ -28,35 +28,3 @@ document.querySelector('.image-profil-telephone').addEventListener('click', func
     behavior: 'smooth'
   });
 });
-// document
-//   .querySelector(".image-profil-telephone")
-//   .addEventListener("click", function (event) {
-//     event.preventDefault();
-
-//     var target = document.querySelector("#right_aside");
-//     var topOffset = window.pageYOffset || document.documentElement.scrollTop;
-//     var targetOffsetTop = target.getBoundingClientRect().top + topOffset;
-//     var startPosition = topOffset;
-//     var distance = targetOffsetTop - startPosition;
-//     var duration = 20000;
-//     var start = null;
-
-//     window.requestAnimationFrame(step);
-
-//     function step(timestamp) {
-//       if (!start) start = timestamp;
-//       var progress = timestamp - start;
-//       window.scrollTo(
-//         0,
-//         easeInOutCubic(progress, startPosition, distance, duration)
-//       );
-//       if (progress < duration) window.requestAnimationFrame(step);
-//     }
-//   });
-
-// function easeInOutCubic(t, b, c, d) {
-//   t /= d / 2;
-//   if (t < 1) return (c / 2) * t * t * t + b;
-//   t -= 2;
-//   return (c / 2) * (t * t * t + 2) + b;
-// }
